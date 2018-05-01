eb3adb4932ec6c0c971283b3cd32f3d13773dbf4
contrbas@gmail.com <K4rmU78B>
2018-05-01 15:53:17 +0300

    c

--- work/srcs/build_graph_edge.c
+++ work/srcs/build_graph_edge.c
@@ -30,10 +30,11 @@ static int		ft_valid_type_of_the_line(int type_of_the_line)
 	return (0);
 }
 
-void 	add_data_to_the_vertex(t_vertex *vertex, char *medium)
+void			add_data_to_the_vertex(t_vertex *vertex, char *medium)
 {
-	t_edge *tmp = vertex->e_next;
+	t_edge *tmp;
 
+	tmp = vertex->e_next;
 	if (!vertex->e_next)
 		vertex->e_next = ft_edge_builder(&medium[1]);
 	else
@@ -42,10 +43,9 @@ void 	add_data_to_the_vertex(t_vertex *vertex, char *medium)
 			tmp = tmp->next;
 		tmp->next = ft_edge_builder(&medium[1]);
 	}
-
 }
 
-t_vertex	*ft_find_vetex_according_to_a_link(t_vertex *vertex, char *str) // find for the second -> two functions or two pointers;
+t_vertex		*ft_find_vetex(t_vertex *vertex, char *str)  // find for the second -> two functions or two pointers;
 {
 	char **medium;
 
@@ -68,24 +68,15 @@ void			ft_build_edge_structure(t_init *initial_data, t_vertex *vertex)
 	t_vertex	*current;
 	char		**medium;
 
-	current = NULL; // maybe delete;
-	medium = NULL; // maybe delete;
 	while (initial_data)
 	{
 		if (ft_valid_type_of_the_line(initial_data->type_of_the_line))
 		{
 			medium = ft_strsplit(initial_data->valid_line, '-');
-
-			current = ft_find_vetex_according_to_a_link(vertex, medium[0]);
-			
+			current = ft_find_vetex(vertex, medium[0]);
 			add_data_to_the_vertex(current, *medium);
-
 			ft_clean_2d_char(medium);
 		}
-		current = NULL; // maybe delete;
-		medium = NULL; // maybe delete;
-		
 		initial_data = initial_data->next;
 	}
 }
-
