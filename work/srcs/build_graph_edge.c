82076b7b9e8c744ee5c7ecabd3eed8495ec96713
contrbas@gmail.com <K4rmU78B>
2018-05-01 14:33:10 +0300

    c

--- work/srcs/build_graph_edge.c
+++ work/srcs/build_graph_edge.c
@@ -30,38 +30,100 @@ static int		ft_valid_type_of_the_line(int type_of_the_line)
 	return (0);
 }
 
-static void		ft_add_data_to_the_edge(t_vertex *vertex, char **medium)
-{
-	if (ft_strequ(vertex->vertex_name, medium[0]))
-		ft_printf("%s\n", "1");
-	else if (ft_strequ(vertex->vertex_name, medium[1]))
-		ft_printf("%s\n", "2");
-}
+// static void		ft_add_data_to_the_edge(t_vertex *vertex, char **medium)
+// {
+// 	if (ft_strequ(vertex->vertex_name, medium[0]))
+// 	{
+// 		if (!vertex->e_next)
+// 		{
+// 			// ft_printf("medium[0] -> %s\n", medium[0]);
+// 			// ft_printf("medium[1] -> %s\n", medium[1]);
 
-static void	ft_add_edge(t_vertex *vertex, char *valid_line)
+// 			vertex->e_next = ft_edge_builder(medium[1]);
+// 			// ft_printf("vertex_name -> %s\n", vertex->e_next->room_name);
+
+			
+// 		}
+// 		else
+// 		{
+
+// 			// ft_printf("medium[0] -> %s\n", medium[0]);
+// 			// ft_printf("medium[1] -> %s\n", medium[1]);
+// 			// ft_printf("vertex_name -> %s\n", vertex->e_next->room_name);
+
+
+			
+// 			while (vertex->e_next->next)
+// 			{
+// 				// ft_printf("%s\n", vertex->e_next->room_name);
+// 				vertex->e_next = vertex->e_next->next;
+// 			}
+// 			vertex->e_next->next = ft_edge_builder(medium[1]);
+// 			// ft_printf("%s\n", vertex->e_next);
+// 		}
+// 	}
+
+
+
+// 	else if (ft_strequ(vertex->vertex_name, medium[1]))
+// 	{
+// 		// if (!vertex->e_next)
+// 			// ft_printf("%s\n", "NULL");
+// 		// ft_printf("%s\n", "2");
+// 	}
+// }
+
+// static void	ft_add_edge(t_vertex *vertex, char *valid_line)
+// {
+// 	char **medium;
+
+// 	medium = ft_strsplit(valid_line, '-');
+// 	while (vertex)
+// 	{
+// 		ft_add_data_to_the_edge(vertex, medium);
+// 		vertex = vertex->v_next;
+// 	}
+// 	ft_clean_2d_char(medium);
+// }
+
+t_vertex	*ft_find_vetex_according_to_a_link(t_vertex *vertex, char *str) // find for the second;
 {
 	char **medium;
 
-	medium = ft_strsplit(valid_line, '-');
 
 	while (vertex)
 	{
-		ft_add_data_to_the_edge(vertex, medium);
-
+		medium = ft_strsplit(vertex->vertex_name, ' ');
+		if (ft_strequ(medium[0], str))
+		{
+			ft_printf("O.K.\n");
+			ft_clean_2d_char(medium);
+			return (vertex);
+		}
+			ft_clean_2d_char(medium);
+		
 		vertex = vertex->v_next;
 	}
-	ft_clean_2d_char(medium);
+	return (NULL);
 }
 
-void			ft_build_edge_structure(t_init *initial_data, t_vertex *g)
+void			ft_build_edge_structure(t_init *initial_data, t_vertex *vertex)
 {
-	// t_edge	*current; // ?
+	t_vertex		*current;
+	char **medium;
 
 	while (initial_data)
 	{
-		if (ft_valid_type_of_the_line(initial_data->type_of_the_line)) // +
+		if (ft_valid_type_of_the_line(initial_data->type_of_the_line))
 		{
-			ft_add_edge(g, initial_data->valid_line);
+			medium = ft_strsplit(initial_data->valid_line, '-');
+			current = ft_find_vetex_according_to_a_link(vertex, medium[0]);
+
+			ft_printf("current->vertex_name%s\n", current->vertex_name);
+
+			ft_clean_2d_char(medium);
+
+			// free;
 		}
 		initial_data = initial_data->next;
 	}
