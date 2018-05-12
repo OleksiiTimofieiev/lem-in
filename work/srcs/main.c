c2416608172502efa8cebd34d59676a08e70bc98
contrbas@gmail.com <K4rmU78B>
2018-05-12 08:55:23 +0300

    c

--- work/srcs/main.c
+++ work/srcs/main.c
@@ -85,9 +85,6 @@ int		main(void)
 	ft_build_vertex_structure(initial_data, &graph);
 	ft_build_edge_structure(initial_data, graph);
 	ft_add_data_to_graph(initial_data, &data);
-
-	// ft_print_lg(graph);  // intermidiaty function;
-
 	bfs(data, graph, &way);
 
 	// while (way) //leaks;
@@ -95,17 +92,8 @@ int		main(void)
 	// 	ft_printf("way node name -> %s\n", way->way_room);
 	// 	way = way->next;
 	// }
-
-	ft_printf("\n");
-	// ft_print_lg(graph);  // intermidiaty function;
-	// ft_print_lg(graph);  // intermidiaty function;
-
 	
+	ft_printf("\n");
 	system("leaks -q lem-in");
-	// while(1)
-	// {
-	// 	;
-	// }
-
 	return (0);
 }
